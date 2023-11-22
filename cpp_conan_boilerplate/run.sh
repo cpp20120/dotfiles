d30ad6204ae19dc4ee12fcece0b38c0c2840b66e
Cppshizoid <alex.krivoschecov@gmail.com>
2023-11-22 10:58:02 +0700

    Update run.sh

--- cpp_conan_boilerplate/run.sh
+++ cpp_conan_boilerplate/run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [ $(dpkg-query -W -f='${Status}' conan 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  sudo apt-get install conan;
+fi
+
+if [ $( rpm -qa --quiet | grep conan 2>/dev/null | grep -c "ok installed") -eq 0]
+  sudo dnf install conan;
+fi
 
 set -e
 set -x
