3bdb9708b98aded874c52b9e36c4bf35dcd53cb8
Cppshizoid <alex.krivoschecov@gmail.com>
2024-02-27 03:09:11 +0700

    Create run.sh

---
+++ cpp_qt_boilerplate/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Проверка типа дистрибутива
+if [ -x "$(command -v apt-get)" ]; then
+    sudo apt-get update
+    sudo apt-get install -y build-essential neofetch git clang clang-tools gcc cmake ninja-build lld lldb valgrind python3-pip doxygen neovim qt6-base-dev qt6-base
+
+elif [ -x "$(command -v dnf)" ]; then
+    sudo dnf install -y @development-tools neofetch git clang clang-tools-extra gcc cmake ninja-build lld lldb valgrind python3-pip doxygen neovim qt6-core qt6-qtbase qt6-qtbase-devel qt6-qtmutimedia  
+
+
+else
+    echo "Не удалось определить дистрибутив и установщик пакетов."
+    exit 1
+fi
+
+pip3 install conan
+
+set -e
+set -x
+
+BASEDIR=$(dirname "$0")
+pushd "$BASEDIR"
+
+rm -rf build
+
+conan install . --output-folder=build --build=missing
+cd build
+cmake .. -G Ninja -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build .
+./test
